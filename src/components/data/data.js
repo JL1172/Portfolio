b2f98d5ed2c2532d38d41c828b79c00d365124d8
Jacob Lang <jacoblang72@comcast.net>
2023-09-19 20:31:44 -0400

    typos

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,6 +1,6 @@
 const Description =  () => {
     return (
-   <div><div id ="firstSpan">I am currently a student at <span className="colorWords">Bloom Institute of Technology</span> for <span className="colorWords">Full-stack web development</span> and I am a
+   <div><div id ="firstSpan">I am currently a student at <span className="colorWords">Bloom Institute of Technology</span> for <span className="colorWords">Full-Stack Web Development</span>, and I am a
     student at <span className="colorWords">GCU</span> pursuing a Bachelors in Accounting public-</div><div id = "secondSpan">accounting.  
     I love not only the technical expertise software engineering demands, but the ability to be hands on with a project that can make a
     difference in the world, in</div><div id = "thirdSpan"> the lives of many or few. I am a team player and highly self-motivated and
