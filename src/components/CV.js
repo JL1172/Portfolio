04c5879f74c4cb0a0a31babf816a07de0642a177
Jacob Lang <jacoblang72@comcast.net>
2023-09-19 20:44:29 -0400

    typos fixed

--- src/components/CV.js
+++ src/components/CV.js
@@ -20,28 +20,28 @@ const CV = (props) => {
                     
                     <div>
                     <div  className="h5h55For">
-                    <h5 className="h5h5">Grand Canyon University <span class='noBoldness'>Phoenix,AZ</span></h5>
+                    <h5 className="h5h5">Grand Canyon University, <span class='noBoldness'>Phoenix, AZ</span></h5>
                     </div>
                     <h5>Accounting with an Emphasis in Public Accounting</h5>
                     <div  className="h5h55For">
                     <h5 className="h5h55" >Bloom Institute of Technology</h5>
                     </div>
-                    <h5>Full-stack Web Development<span class='noBoldness'> Computer Science and Programming</span></h5>
+                    <h5>Full-Stack Web Development<span class='noBoldness'> Computer Science and Programming</span></h5>
                     </div>
                 </div>
                 <div className="mainContent">
                 <h3 class ='h4'>{"<"}Work History{">"}</h3>
                 <div id='electrical'>
-                    <h4 className="h5h56" style = {{margin : "0"}}>In-house Electrician</h4>
+                    <h4 className="h5h56" style = {{margin : "0"}}>In-House Electrician</h4>
                     <p>02/2023 - 04/2023</p>
-                    <h5 >CFTN, Phoenix, Arizona</h5>
+                    <h5 >Church For The Nations, Phoenix, Arizona</h5>
                     <ul>
-                        <li>Worked from ladders, scaffold, and lifts to install, maintain, or repair electrical wiring
+                        <li>Repaired electrical wiring
                             equipment along with 
                             other necessary maintenance.</li>
-                        <li>PM on service panels, wired service panels and circuit breakers.</li>
-                        <li>Planned layout and installation of electrical wiring following best practices and in
-                            compliance with NEC.</li>
+                        <li>Preventative maintenance on service panels, wired service panels and circuit breakers.</li>
+                        <li>Created electrical plan layout and executed installation of electrical wiring following best practices and in
+                            compliance with NEC (National Electric Code).</li>
                     </ul>
 
                 </div>
@@ -51,7 +51,7 @@ const CV = (props) => {
                     <h4>Thomas Furniture Restoration, Kennet Square, PA</h4>
                     <ul>
                         <li>Completed on time deliveries while also delivering antique and luxury furniture pieces.</li>
-                        <li>Aided shop personnel with furniture restoration.</li>
+                        <li>Furniture restoration.</li>
                     </ul>
                 </div>
                 </div>
